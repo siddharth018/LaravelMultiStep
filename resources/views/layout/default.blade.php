0e5cf5631785c4450c13f8c16339d3e634463df6
siddharth018 <siddharthshukla089@gmail.com>
2020-08-02 14:36:08 +0530

    real programmer logo

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -5,7 +5,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
 </head>
-<body style="background-color: #f3fdf3">
+<body style="background-color: #f4433647">
     <div class="container-fluid bg-1 text-center">
     <img src="https://realprogrammer.in/wp-content/uploads/2019/10/Screenshot-2019-10-19-at-12.50.58-AM.png" class="img-responsive img-circle margin" style="display:inline" alt="Bird" width="20%">
 </div>  
